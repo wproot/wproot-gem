edd2a4017d42dec1235847dc6fb6d20c9ef345d0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-11-02 17:32:02 +0530

    Make haml work.

--- lib/wproot/cli.rb
+++ lib/wproot/cli.rb
@@ -11,18 +11,27 @@ module WPRoot
       puts "Watching #{directory}"
       FSSM.monitor do
         path directory do
-          glob '*.php.haml'
+          glob '**/*.php.haml'
 
           compile = lambda do |base, relative|
+            path = File.join(base, relative)
+            dir = File.dirname(path)
+            new_path = File.join(dir, File.basename(path, '.haml'))
+            File.open(new_path, 'w') do |f|
+              f.write(Haml.new(File.read(path)).compile)
+              puts ">>> Compiled #{path} to #{new_path}"
+            end
           end
           update &compile
           create &compile
         end
 
         path directory do
-          glob '*.css.sass'
+          glob '**/*.css.sass'
 
           compile = lambda do |base, relative|
+            path = File.join(base, relative)
+            p path
           end
           update &compile
           create &compile
