71dc783da0bf25d99e666213b0b2da274ffdc391
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-11-02 19:36:00 +0530

    Compile existing files on calling watch.

--- lib/wproot/cli.rb
+++ lib/wproot/cli.rb
@@ -9,24 +9,29 @@ module WPRoot
 
     def watch(directory)
       puts "Watching #{directory}"
-      FSSM.monitor directory do
-        compile = lambda do |base, relative|
-          path = File.join(base, relative)
-          dir = File.dirname(path)
-          if File.extname(relative) == '.haml'
-            new_path = File.join(dir, File.basename(path, '.haml'))
-            File.open(new_path, 'w') do |f|
-              f.write(Haml.new(File.read(path)).compile)
-              puts ">>> Compiled #{path} to #{new_path}"
-            end
-          elsif File.extname(relative) == '.sass'
-            new_path = File.join(dir, File.basename(path, '.sass'))
-            File.open(new_path, 'w') do |f|
-              f.write(Compass.new(File.read(path)).compile)
-              puts ">>> Compiled #{path} to #{new_path}"
-            end
+      compile = lambda do |base, relative|
+        path = File.join(base, relative)
+        dir = File.dirname(path)
+        if File.extname(relative) == '.haml'
+          new_path = File.join(dir, File.basename(path, '.haml'))
+          File.open(new_path, 'w') do |f|
+            f.write(Haml.new(File.read(path)).compile)
+            puts ">>> Compiled #{path} to #{new_path}"
+          end
+        elsif File.extname(relative) == '.sass'
+          new_path = File.join(dir, File.basename(path, '.sass'))
+          File.open(new_path, 'w') do |f|
+            f.write(Compass.new(File.read(path)).compile)
+            puts ">>> Compiled #{path} to #{new_path}"
           end
         end
+      end
+
+      Dir["**/*"].each do |f|
+        compile.call(File.dirname(f), File.basename(f))
+      end
+
+      FSSM.monitor directory do
         update &compile
         create &compile
       end
