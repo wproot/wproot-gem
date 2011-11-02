320a66a438940b435d6f94cb368604d478deedd1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-11-02 19:09:50 +0530

    Make it all work.

--- lib/wproot/cli.rb
+++ lib/wproot/cli.rb
@@ -9,39 +9,26 @@ module WPRoot
 
     def watch(directory)
       puts "Watching #{directory}"
-      FSSM.monitor do
-        path directory do
-          glob '**/*.php.haml'
-
-          compile = lambda do |base, relative|
-            path = File.join(base, relative)
-            dir = File.dirname(path)
+      FSSM.monitor directory do
+        compile = lambda do |base, relative|
+          path = File.join(base, relative)
+          dir = File.dirname(path)
+          if File.extname(relative) == '.haml'
             new_path = File.join(dir, File.basename(path, '.haml'))
             File.open(new_path, 'w') do |f|
               f.write(Haml.new(File.read(path)).compile)
               puts ">>> Compiled #{path} to #{new_path}"
             end
-          end
-          update &compile
-          create &compile
-        end
-
-        path directory do
-          glob '**/*.css.sass'
-
-          compile = lambda do |base, relative|
-            path = File.join(base, relative)
-            dir = File.dirname(path)
+          elsif File.extname(relative) == '.sass'
             new_path = File.join(dir, File.basename(path, '.sass'))
             File.open(new_path, 'w') do |f|
               f.write(Compass.new(File.read(path)).compile)
               puts ">>> Compiled #{path} to #{new_path}"
             end
           end
-          update &compile
-          create &compile
         end
-
+        update &compile
+        create &compile
       end
     end
   end
